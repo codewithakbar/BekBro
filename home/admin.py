9ce5f6f51fca480fb26a0f41f60a0a49e2a7d85c
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-07-29 13:58:56 +0500

    admin and views

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+        AboutMe, Education, Experience, PortfolioCategory, Portfolio, Service, 
+        Testimonials, Blog, Contact
+    )
+
+
+@admin.register(AboutMe)
+class AboutMeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PortfolioCategory)
+class PortfolioCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Testimonials)
+class TestimonialsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 from .models import AboutMe, Education, Experience, PortfolioCategory, Portfolio, Service, Testimonials, Blog, Contact
 from .serializers import (
     AboutMeSerializer,
@@ -14,45 +16,54 @@ from .serializers import (
 
 
 class AboutMeViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = AboutMe.objects.all()
     serializer_class = AboutMeSerializer
 
 
 class EducationViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Education.objects.all()
     serializer_class = EducationSerializer
 
 
 class ExperienceViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Experience.objects.all()
     serializer_class = ExperienceSerializer
 
 
 class PortfolioCategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = PortfolioCategory.objects.all()
     serializer_class = PortfolioCategorySerializer
 
 
 class PortfolioViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Portfolio.objects.all()
     serializer_class = PortfolioSerializer
 
 
 class ServiceViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 
 class TestimonialsViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Testimonials.objects.all()
     serializer_class = TestimonialsSerializer
 
 
 class BlogViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
 
 class ContactViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
